8421d68370a0d8d0d16ce409c799015332191578
Alan Costa <alancoosta2323@gmail.com>
2021-11-24 07:49:15 -0300

    [PLGR-01234][PLGR-56789] Change make file and git hooks

--- ci/hooks/commit-msg.sh
+++ ci/hooks/commit-msg.sh
@@ -1,5 +1,5 @@
-commit_regex='(\[BEESSPR-[0-9]+\]\[BEESSPR-[0-9]+\]|merge)'
-error_msg="ERROR! Commit message should follow the pattern [BEESSPR-1234][BEESSPR-7890], where 1234 is the code of the story and 7890 is the code of the task."
+commit_regex='(\[PLGR-[0-9]+\]\[PLGR-[0-9]+\]|merge)'
+error_msg="ERROR! Commit message should follow the pattern [PLGR-1234][PLGR-7890], where 1234 is the code of the story and 7890 is the code of the task."
 
 if ! grep -iqE "$commit_regex" "$1"; then
     echo "$error_msg" >&2
