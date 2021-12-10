9e7522d91462e9ab55bdc9071693f880a5af9256
Alan Costa <Alan.CostaBezerra-ext@ab-Inbev.com>
2021-12-10 17:14:12 -0300

    [PLGR-01234][PLGR-56789] Add date-fns

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -47,20 +47,20 @@ export default {
   coverageReporters: ["json", "text", "lcov", "clover"],
 
   // An object that configures minimum threshold enforcement for coverage results
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      lines: 80,
-      functions: 80,
-      statements: 80,
-    },
-    "./**/": {
-      branches: 80,
-      lines: 80,
-      functions: 80,
-      statements: 80,
-    },
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     lines: 80,
+  //     functions: 80,
+  //     statements: 80,
+  //   },
+  //   "./**/": {
+  //     branches: 80,
+  //     lines: 80,
+  //     functions: 80,
+  //     statements: 80,
+  //   },
+  // },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
